5306fbd2d888b3dc7d022bef1bfae6faf1dab2f6
Kian Arven Valencia <arvenkian1234@gmail.com>
2025-03-08 22:26:46 +0800

    fixed resolution to dynamic and added new background for living room

--- LivingRoom.cs
+++ LivingRoom.cs
@@ -2,11 +2,11 @@ using Godot;
 
 public partial class LivingRoom : Node2D
 {
-	private AnimatedSprite2D animatedSprite;
+	/*private AnimatedSprite2D animatedSprite;
 
 	public override void _Ready()
 	{
 		animatedSprite = GetNode<AnimatedSprite2D>("AnimatedSprite2D");
 		animatedSprite.Play("default"); // Play animation on scene load
-	}
+	}*/
 }
