8654f79e5792c174b612195eb33a048621988527
Gerry Vien Flores <gerryvienlifeflores@gmail.com>
2025-04-24 07:50:31 +0800

    Reaction

--- scripts/MainMenu.cs
+++ scripts/MainMenu.cs
@@ -3,11 +3,23 @@ using Godot;
 public partial class MainMenu : Node2D
 {
 	private AudioStreamPlayer2D musicPlayer;
+	private TextureRect ratingsPnl;
+	private Button rateBtn;
+	private Button closeRatings;
+	private ButtonGroup react;
 
 	public override void _Ready()
 	{
 		// Get the AudioStreamPlayer2D node
 		musicPlayer = GetNode<AudioStreamPlayer2D>("kahoot_jungle");
+		ratingsPnl = GetNode<TextureRect>("LandingScene/RatingPanel");
+		rateBtn = GetNode<Button>("LandingScene/rateBtn");
+		closeRatings = GetNode<Button>("LandingScene/RatingPanel/close_button");
+		
+		rateBtn.Pressed += showRatings;
+		closeRatings.Pressed += closeRating;
+		
+		react =  GD.Load<ButtonGroup>("res://react.tres");
 
 		// Play the music
 		if (!musicPlayer.Playing)
@@ -15,9 +27,22 @@ public partial class MainMenu : Node2D
 			musicPlayer.Play();
 		}
 	}
+	
+	private void showRatings()
+	{
+		ratingsPnl.Position = new Vector2(-146,-176);
+		ratingsPnl.Visible = true;
+	}
+	
+	private void closeRating()
+	{
+		ratingsPnl.Visible = false;
+	}
 
 	public override void _Input(InputEvent @event)
 	{
 		// Handle input events if needed
 	}
+	
+
 }
