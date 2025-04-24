49c6240ea1b3b7391b46362e07061f2a6afd45b3
Gerry Vien Flores <gerryvienlifeflores@gmail.com>
2025-04-24 18:38:18 +0800

    Ratings Logic, No Design, No Storing

--- scripts/MainMenu.cs
+++ scripts/MainMenu.cs
@@ -1,4 +1,5 @@
 using Godot;
+using System.Collections.Generic;
 
 public partial class MainMenu : Node2D
 {
@@ -7,6 +8,16 @@ public partial class MainMenu : Node2D
 	private Button rateBtn;
 	private Button closeRatings;
 	private ButtonGroup react;
+	private ButtonGroup ease;
+	private ButtonGroup effectivity;
+	private ButtonGroup learn;
+	private ButtonGroup satisfaction;
+	private Control reactGroup;
+	private Control easeGroup;
+	private Control effectivityGroup;
+	private Control learnGroup;
+	private Control satisfactionGroup;
+	private List<string> ratings;
 
 	public override void _Ready()
 	{
@@ -15,11 +26,28 @@ public partial class MainMenu : Node2D
 		ratingsPnl = GetNode<TextureRect>("LandingScene/RatingPanel");
 		rateBtn = GetNode<Button>("LandingScene/rateBtn");
 		closeRatings = GetNode<Button>("LandingScene/RatingPanel/close_button");
+		ratings = new List<string>{"", "", "", "", ""};
 		
 		rateBtn.Pressed += showRatings;
 		closeRatings.Pressed += closeRating;
 		
-		react =  GD.Load<ButtonGroup>("res://react.tres");
+		react =  GD.Load<ButtonGroup>("res://groups/react.tres");
+		ease =  GD.Load<ButtonGroup>("res://groups/ease.tres");
+		effectivity =  GD.Load<ButtonGroup>("res://groups/effectivity.tres");
+		learn =  GD.Load<ButtonGroup>("res://groups/learn.tres");
+		satisfaction =  GD.Load<ButtonGroup>("res://groups/satisfaction.tres");
+		
+		reactGroup = GetNode<Control>("LandingScene/RatingPanel/React");
+		effectivityGroup = GetNode<Control>("LandingScene/RatingPanel/Effectivity");
+		easeGroup = GetNode<Control>("LandingScene/RatingPanel/Ease");
+		learnGroup = GetNode<Control>("LandingScene/RatingPanel/Learn");
+		satisfactionGroup = GetNode<Control>("LandingScene/RatingPanel/Satisfaction");
+		
+		cbAction(react, reactGroup);
+		cbAction(effectivity, effectivityGroup);
+		cbAction(ease, easeGroup);
+		cbAction(learn, learnGroup);
+		cbAction(satisfaction, satisfactionGroup);
 
 		// Play the music
 		if (!musicPlayer.Playing)
@@ -44,5 +72,40 @@ public partial class MainMenu : Node2D
 		// Handle input events if needed
 	}
 	
+	private void cbAction(ButtonGroup buttonGroup, Control groupParent)
+	{
+		foreach (Node child in groupParent.GetChildren())
+		{
+			if (child is CheckButton cb)
+			{
+				// Force assign the correct ButtonGroup
+				cb.ButtonGroup = buttonGroup;
 
+				GD.Print($"Connecting: {cb.Name} to group {buttonGroup.ResourcePath}");
+				cb.Toggled += (bool pressed) => OnButtonToggled(pressed, buttonGroup, groupParent);
+			}
+		}
+	}
+
+	private void OnButtonToggled(bool pressed, ButtonGroup buttonGroup, Control groupParent)
+	{
+		if (!pressed)
+			return;
+
+		foreach (Node child in groupParent.GetChildren())
+		{
+			if (child is CheckButton cb && cb.ButtonGroup == buttonGroup && cb.ButtonPressed)
+			{
+				GD.Print($"Selected: {cb.Name}");
+				identifyGroup(cb);
+			}
+		}
+	}
+	
+	private void identifyGroup(CheckButton cb){
+		List<string> groupDir = new List<string>{"res://groups/react.tres", "res://groups/effectivity.tres", "res://groups/ease.tres", "res://groups/learn.tres", "res://groups/satisfaction.tres"};
+		ratings[groupDir.IndexOf(cb.ButtonGroup.ResourcePath)] = cb.Name;
+		string result = string.Join(", ", ratings);
+		GD.Print(result);
+	}
 }
